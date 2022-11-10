e38d2ae0413761a76a9faae0e69787bf6a067ebe
yesjuhee <juhee0924@g.skku.edu>
2022-11-11 02:38:43 +0900

    2775번 : 최종, n을 다루는 방법을 바꿔서 코드를 깔끔하게 고침

--- StepByStep/7-BasicMath1/2775.py
+++ StepByStep/7-BasicMath1/2775.py
@@ -1,13 +1,10 @@
 # 부녀회장이 될테야
 
-# 테이블 초기화 : size 15 x 14
-dp_table = [[0]*14 for _ in range(15)]
-
-
-def dp(k, n):  # DP 함수
+# DP 함수
+def dp(k, n):
     if k == 0:
-        return n+1
-    if n == 0:
+        return n
+    if n == 1:
         return 1
     if dp_table[k][n] != 0:
         return dp_table[k][n]
@@ -16,9 +13,12 @@ def dp(k, n):  # DP 함수
     return dp_table[k][n]
 
 
+# 테이블 초기화 : size 15 x 15
+dp_table = [[0]*15 for _ in range(15)]
+
 # 입력 및 출력
 t = int(input())
 for _ in range(t):
     k = int(input())
     n = int(input())
-    print(dp(k, n-1))
+    print(dp(k, n))
