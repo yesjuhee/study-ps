3e2bc98d59d61cc76ae2a389f6806cae93e73189
yesjuhee <juhee0924@g.skku.edu>
2022-11-11 02:32:43 +0900

    2775번 : DP 함수 구현

--- StepByStep/7-BasicMath1/2775.py
+++ StepByStep/7-BasicMath1/2775.py
@@ -3,7 +3,17 @@
 # 테이블 초기화 : size 15 x 14
 dp_table = [[0]*14 for _ in range(15)]
 
-# DP 함수
+
+def dp(k, n):  # DP 함수
+    if k == 0:
+        return n+1
+    if n == 0:
+        return 1
+    if dp_table[k][n] != 0:
+        return dp_table[k][n]
+
+    dp_table[k][n] = dp(k, n-1) + dp(k-1, n)
+    return dp_table[k][n]
 
 
 # 입력 및 출력
@@ -11,4 +21,4 @@ t = int(input())
 for _ in range(t):
     k = int(input())
     n = int(input())
-    print(dp_table(k, n-1))
+    print(dp(k, n-1))
