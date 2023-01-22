6b3087fd83d8cad13caee19587781814f7f86bd1
yesjuhee <juhee0924@g.skku.edu>
2023-01-22 23:36:29 +0900

    selection_sort 수정

--- week4/selection_sort.py
+++ week4/selection_sort.py
@@ -1,10 +1,11 @@
 array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 
-for i in range(len(array)):
+for i in range(len(array)):  # i는 0부터 마지막 인덱스까지, 정렬이 될 원소를 가리킴
     min_index = i
-    for j in range(i + 1, len(array)):
-        if array[min_index] > array[j]:
+    for j in range(i + 1, len(array)):  # 0 ~ i-1 까지는 정렬이 됐으므로 그 뒷 부분을 확인
+        if array[min_index] > array[j]:  # j 인덱스로 끝까지 돌면서 최소값을 선택
             min_index = j
+    # i의 위치에 선택한 최소값으로 swap
     array[i], array[min_index] = array[min_index], array[i]
 
 print(array)
