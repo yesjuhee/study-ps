0101dbcf50f7527484d0b2b9c14f003bf9da7e6c
yesjuhee <juhee0924@g.skku.edu>
2023-02-08 00:00:00 +0900

    진짜 추가

---
+++ week6/fibonacci-bottomup.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+long long d[100];
+
+int main(void)
+{
+    d[1] = 1;
+    d[2] = 2;
+    int n = 50;
+
+    for (int i = 3; i <= n; i++)
+    {
+        d[i] = d[i - 1] + d[i - 2];
+    }
+    cout << d[n] << '\n';
+    return 0;
+}
